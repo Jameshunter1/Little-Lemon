18621322634e86aad80b4634685b6dbe885d11e3
James Hunter <jameshunter1017@live.com>
2023-02-19 16:43:33 -0500

    Meta tags and html setup for nav and footer

---
+++ src/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from "./logo.svg"
+const Nav = () => {
+  return (
+    <nav>
+        <img src={logo} alt="logo"/>
+        <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#menu">Menu</a></li>
+            <li><a href="#reservation">Reservations</a></li>
+            <li><a href="#order">Order Online</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
